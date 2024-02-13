d23876e70cac33c61aebd9ce3c4fe32034a0fa98
Prem Nalla <premnalla@gmail.com>
2024-02-13 13:14:31 -0500

    trying out slog with front-end logger and back-end zap, logrus, zerolog, etc

--- go/cmd/myapp-1/main.go
+++ go/cmd/myapp-1/main.go
@@ -16,8 +16,185 @@ limitations under the License.
 
 package main
 
-import "log"
+import (
+	"github.com/rs/zerolog"
+	sloglogrus "github.com/samber/slog-logrus/v2"
+	slogzap "github.com/samber/slog-zap/v2"
+	"github.com/samber/slog-zerolog"
+	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
+	"go.uber.org/zap/exp/zapslog"
+
+	"net"
+	"net/http"
+
+	"log/slog"
+	"os"
+)
 
 func main() {
-	log.Printf("hello, world!")
+	//log.Printf("hello, world!")
+	//log.Print("Info message")
+	//slog.Info("Info message")
+
+	handlerJson := slog.NewJSONHandler(os.Stdout, nil)
+	logger := slog.NewLogLogger(handlerJson, slog.LevelDebug)
+	logger.Printf("Info message %d", 12345) // doesn't work/show-up
+
+	loggerJson := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(loggerJson)
+	//loggerJson.Debug("Debug message")
+	loggerJson.Info("Info message")
+	//loggerJson.Warn("Warning message")
+	//loggerJson.Error("Error message")
+
+	loggerTxt := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(loggerTxt)
+	//loggerTxt.Debug("Debug message")
+	loggerTxt.Info("Info message")
+	//loggerTxt.Warn("Warning message")
+	//loggerTxt.Error("Error message")
+	//slog.Debug("Debug message")
+	//slog.Info("Info message")
+	slog.Warn("Warning message")
+	//slog.Error("Error message")
+
+	InitLog()
+
+	InitLogZap()
+
+	InitLogZapSamber()
+
+	InitLogZerolog()
+
+	InitLogLogrus()
+
+}
+
+func InitLog( /* out io.Writer, handler slog.Handler */ ) {
+
+}
+
+/**
+ * Ref: https://betterstack.com/community/guides/logging/logging-in-go/
+ */
+func InitLogZap() {
+	zapL := zap.Must(zap.NewProduction())
+
+	defer zapL.Sync()
+
+	logger := slog.New(zapslog.NewHandler(zapL.Core(), nil))
+
+	//logger.Info(
+	//	"incoming request",
+	//	slog.String("method", "GET"),
+	//	slog.String("path", "/api/user"),
+	//	slog.Int("status", 200),
+	//)
+
+	logger.Error("something went wrong", net.ErrClosed,
+		"status", http.StatusInternalServerError)
+
+	//slog.Error("something went wrong", net.ErrClosed,
+	//	"status", http.StatusInternalServerError)
+
+	slog.SetDefault(logger)
+
+	//slog.Debug("Debug message")
+	//slog.Info("Info message")
+	//slog.Warn("Warning message")
+	//slog.Error("Error message")
+	slog.Error("something went wrong", net.ErrClosed,
+		"status", http.StatusInternalServerError)
+
+	//log.Print("something went wrong")
+	//log.Fatalln("something went wrong")
+
+}
+
+// https://github.com/samber/slog-zap/blob/main/example/example.go
+func InitLogZapSamber() {
+	zapLogger, _ := zap.NewProduction()
+
+	logger := slog.New(slogzap.Option{Level: slog.LevelDebug, Logger: zapLogger}.NewZapHandler())
+	//logger = logger.With("release", "v1.0.0")
+
+	//logger.Info(
+	//	"incoming request",
+	//	slog.String("method", "GET"),
+	//	slog.String("path", "/api/user"),
+	//	slog.Int("status", 200),
+	//)
+
+	logger.Error("something went wrong", net.ErrClosed,
+		"status", http.StatusInternalServerError)
+
+	slog.SetDefault(logger)
+
+	//slog.Debug("Debug message")
+	//slog.Info("Info message")
+	//slog.Warn("Warning message")
+	//slog.Error("Error message")
+	slog.Error("something went wrong", net.ErrClosed,
+		"status", http.StatusInternalServerError)
+
+	//log.Print("something went wrong")
+	//log.Fatalln("something went wrong")
+}
+
+/**
+ * Ref: https://betterstack.com/community/guides/logging/logging-in-go/
+ */
+func InitLogZerolog() {
+	zerologL := zerolog.New(os.Stdout).Level(zerolog.InfoLevel)
+
+	logger := slog.New(
+		slogzerolog.Option{Logger: &zerologL}.NewZerologHandler(),
+	)
+
+	//logger.Info(
+	//	"incoming request",
+	//	slog.String("method", "GET"),
+	//	slog.String("path", "/api/user"),
+	//	slog.Int("status", 200),
+	//)
+
+	logger.Error("something went wrong", net.ErrClosed,
+		"status", http.StatusInternalServerError)
+
+	slog.SetDefault(logger)
+
+	//slog.Debug("Debug message")
+	//slog.Info("Info message")
+	//slog.Warn("Warning message")
+	//slog.Error("Error message")
+	slog.Error("something went wrong", net.ErrClosed,
+		"status", http.StatusInternalServerError)
+
+	//log.Print("something went wrong")
+	//log.Fatalln("something went wrong")
+
+}
+
+// https://github.com/samber/slog-logrus/blob/main/example/example.go
+func InitLogLogrus() {
+	logrusLogger := logrus.New()
+
+	logger := slog.New(sloglogrus.Option{Level: slog.LevelDebug, Logger: logrusLogger}.NewLogrusHandler())
+	//logger = logger.With("release", "v1.0.0")
+
+	logger.Error("something went wrong", net.ErrClosed,
+		"status", http.StatusInternalServerError)
+
+	slog.SetDefault(logger)
+
+	//slog.Debug("Debug message")
+	//slog.Info("Info message")
+	//slog.Warn("Warning message")
+	//slog.Error("Error message")
+	slog.Error("something went wrong", net.ErrClosed,
+		"status", http.StatusInternalServerError)
+
+	//log.Print("something went wrong")
+	//log.Fatalln("something went wrong")
 }
--- go/go.mod
+++ go/go.mod
@@ -1,12 +1,30 @@
 module github.com/severalnines/clustercontrol-client-sdk/go
 
-go 1.17
+go 1.21
 
-require golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
+toolchain go1.21.6
+
+require (
+	github.com/rs/zerolog v1.32.0
+	github.com/samber/slog-logrus/v2 v2.2.0
+	github.com/samber/slog-zap/v2 v2.3.0
+	github.com/samber/slog-zerolog v1.0.0
+	github.com/sirupsen/logrus v1.9.3
+	go.uber.org/zap v1.26.0
+	go.uber.org/zap/exp v0.2.0
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
+)
 
 require (
 	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/samber/lo v1.38.1 // indirect
+	github.com/samber/slog-common v0.14.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 )
