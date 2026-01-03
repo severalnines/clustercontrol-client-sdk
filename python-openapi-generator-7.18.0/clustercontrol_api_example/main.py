e0fb46277004e471836ccb52e7fa45b2bd5fa10a
Prem Nalla <premnalla@gmail.com>
2026-01-03 16:43:09 -0500

    new python binding based on openapi generator 7.18.0

---
+++ python-openapi-generator-7.18.0/clustercontrol_api_example/main.py
@@ -0,0 +1,12 @@
+from clustercontrol_api_example import authenticate
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+print("Starting....")
+
+authenticate.authenticateWithCC()
+authenticate.getClusterInfo()
+authenticate.getAllClusterInfo()
+
