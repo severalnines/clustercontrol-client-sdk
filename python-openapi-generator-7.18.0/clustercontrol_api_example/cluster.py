61de2d28c4b9300cab3a2a429bb81944245a928e
Prem Nalla <premnalla@gmail.com>
2026-01-06 14:54:26 -0500

    removed commented code

--- python-openapi-generator-7.18.0/clustercontrol_api_example/cluster.py
+++ python-openapi-generator-7.18.0/clustercontrol_api_example/cluster.py
@@ -1,36 +1,25 @@
-import openapi_cc_client
 import client
 import openapi_cc_client
-from openapi_cc_client import JobsJobJobSpecJobDataNodesInner
 from openapi_cc_client.models.jobs import Jobs
 from openapi_cc_client.models.jobs_job import JobsJob
 from openapi_cc_client.models.jobs_job_job_spec import JobsJobJobSpec
 from openapi_cc_client.models.jobs_job_job_spec_job_data import JobsJobJobSpecJobData
 from openapi_cc_client.models.jobs_job_job_spec_job_data_nodes_inner import JobsJobJobSpecJobDataNodesInner
-# from openapi_cc_client.rest import ApiException
-# from pprint import pprint
 
 def createCluster():
     with openapi_cc_client.ApiClient(client.configuration) as api_client:
         api_instance = openapi_cc_client.JobsApi(api_client)
-        #jobs = openapi_cc_client.Jobs  # Jobs | Creating, manipulating, and obtaining information about jobs
         jobs = Jobs(
             operation='createJobInstance',
         )
-        #jobs.operation = "create_cluster"
 
-        #job = jobs.job
         job = JobsJob(
             class_name="CmonJobInstance",
         )
-        #job.class_name = "CmonJobInstance"
 
-        #jobSpec = openapi_cc_client.JobsJobJobSpec()
         jobSpec = JobsJobJobSpec(
             command="create_cluster",
         )
-        #jobSpec.command = "create_cluster"
-        # jobSpecData = openapi_cc_client.JobsJobJobSpecJobData()
         jobSpecData = JobsJobJobSpecJobData(
             cluster_name="test-cluster",
             cluster_type="postgresql_single",
@@ -49,7 +38,6 @@ def createCluster():
             use_internal_repos=False,
             port=5432,
         )
-        #jobSpecData.cluster_name = "test-cluster"
         node = JobsJobJobSpecJobDataNodesInner(
             hostname="10.0.0.189",
             port="5432",
