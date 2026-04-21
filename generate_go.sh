edb3feeb57aac88fdb38a4081ecc02fdb3470e33
Prem Nalla <premnalla@gmail.com>
2026-04-20 20:12:48 -0400

    CLUS-7344 - part-I commit

--- generate_go.sh
+++ generate_go.sh
@@ -4,11 +4,11 @@ GODIR=./generated/go
 echo "Cleaning up GoLang code in $GODIR"
 /bin/rm -rf $GODIR
 echo "Generating GoLang code in $GODIR"
-openapi-generator generate --input-spec ./clustercontrol-v2.yaml --generator-name go --output $GODIR
+openapi-generator generate --input-spec ./clustercontrol-v2.yaml --generator-name go --output $GODIR --git-user-id severalnines --git-repo-id clustercontrol-client-sdk/go/pkg/openapi
 #
 /bin/rm -rf ./go/pkg/openapi
-#/bin/rm -rf ./go/pkg/openapi/test
+/bin/rm -rf ./go/pkg/openapi/test
 cp -rp $GODIR ./go/pkg/openapi
-#cp -rp $GODIR/test ./go/pkg/openapi/
-#cp -rp $GODIR/docs ./go/pkg/openapi/
+cp -rp $GODIR/test ./go/pkg/openapi/
+cp -rp $GODIR/docs ./go/pkg/openapi/
 #
--- generated/go/go.mod
+++ generated/go/go.mod
@@ -1,4 +1,4 @@
-module github.com/GIT_USER_ID/GIT_REPO_ID
+module github.com/severalnines/clustercontrol-client-sdk/go/pkg/openapi
 
 go 1.23
 
