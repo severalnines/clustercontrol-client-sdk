7c613dccac5c98361ff4cbc83c6dbefb69a1d101
Prem Nalla <premnalla@gmail.com>
2026-04-22 16:47:57 -0400

    Refining go genration, copy over and cleanup

--- generate_go.sh
+++ generate_go.sh
@@ -7,11 +7,23 @@ echo "Generating GoLang code in $GODIR"
 openapi-generator generate --input-spec ./clustercontrol-v2.yaml --generator-name go --output $GODIR --git-user-id severalnines --git-repo-id clustercontrol-client-sdk/go/pkg/openapi
 #
 DEST_BASE=./go/pkg/openapi
-/bin/rm -rf $DEST_BASE
-cp -rp $GODIR $DEST_BASE
 #
-echo "Cleaning up unwanted files"
-rm go/pkg/openapi/.gitignore
-git restore go/pkg/openapi/go.mod
-git restore go/pkg/openapi/go.sum
+# Cleanup files
+echo "Cleaning up old content from $DEST_BASE - source code, test code and docs"
+/bin/rm $DEST_BASE/*.go
+/bin/rm $DEST_BASE/test/*.go
+/bin/rm $DEST_BASE/docs/*.md
+# Copy over newly generated files
+echo "Copying over newly generated content to $DEST_BASE - source code, test code and docs"
+cp $GODIR/*.go $DEST_BASE
+cp -rp $GODIR/test $DEST_BASE
+cp -rp $GODIR/docs $DEST_BASE
+#
+echo "Done."
+# cp -rp $GODIR $DEST_BASE
+# #
+# echo "Cleaning up unwanted files"
+# rm go/pkg/openapi/.gitignore
+# git restore go/pkg/openapi/go.mod
+# git restore go/pkg/openapi/go.sum
 #
---
+++ generated/go/go.mod
@@ -0,0 +1,6 @@
+module github.com/severalnines/clustercontrol-client-sdk/go/pkg/openapi
+
+go 1.23
+
+require (
+)
