733d0824df20946b842eef560ceecfb9fbee3ca7
Prem Nalla <premnalla@gmail.com>
2026-04-22 16:57:22 -0400

    Added CC Proxy GO bindings - INITIAL

--- generate_proxy_go.sh
+++ generate_proxy_go.sh
@@ -7,6 +7,17 @@ echo "Generating GO code in $GODIR"
 openapi-generator generate --input-spec ./clustercontrol-proxy.yaml --generator-name go --output $GODIR --git-user-id severalnines --git-repo-id clustercontrol-client-sdk/go/proxy/pkg/openapi
 #
 DEST_BASE=./go/proxy/pkg/openapi
-/bin/rm -rf .$DEST_BASE
-cp -rp $GODIR $DEST_BASE
+#
+# Cleanup files
+echo "Cleaning up old content from $DEST_BASE - source code, test code and docs"
+/bin/rm $DEST_BASE/*.go
+/bin/rm $DEST_BASE/test/*.go
+/bin/rm $DEST_BASE/docs/*.md
+# Copy over newly generated files
+echo "Copying over newly generated content to $DEST_BASE - source code, test code and docs"
+cp $GODIR/*.go $DEST_BASE
+cp -rp $GODIR/test $DEST_BASE
+cp -rp $GODIR/docs $DEST_BASE
+#
+echo "Done."
 #
